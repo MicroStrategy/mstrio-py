6f78cb97fcdf5c6e222c67379db2f7ef4708e625
Adam Piotrowski <apiotrowski@microstrategy.com>
2021-07-15 08:22:34 +0200

    Improve Import Path & Update Examples - cube_report examples typo removed - Application Objects import path improved (aka shortened) (no regression issue)

--- mstrio/application_objects/__init__.py
+++ mstrio/application_objects/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-from . import datasets
+from .datasets import *
 from .document import Document, list_documents, list_documents_across_projects
 from .dossier import Dossier, list_dossiers, list_dossiers_across_projects
 from .library import Library
