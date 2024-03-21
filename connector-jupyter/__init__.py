1a89ff864ca57c2de7e528ccca4bb5d6384c46cd
Gorski, Michal <mgorski@microstrategy.com>
2024-03-21 11:01:27 +0100

    remove connector-jupyter from mstrio-py package

--- connector-jupyter/__init__.py
+++
@@ -1,9 +0,0 @@
-def _jupyter_nbextension_paths():
-    return [
-        dict(
-            section="notebook",
-            src="production/mstr_jupyter/static",
-            dest="mstr_jupyter",
-            require="mstr_jupyter/main",
-        )
-    ]
